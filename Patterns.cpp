04859ecf76a36db792b009b1ed04fe081798e066
jagadish011 <munavallijagadish@gmail.com>
2024-08-09 09:04:27 +0530

    changes in patterns cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -49,13 +49,32 @@ using namespace std;
 // 1 2 3 4
 // 1 2 3 4 5
 // 1 2 3 4 5 6
-void pattern3(int n )
+// void pattern3(int n )
+// {
+//     for(int i=1; i<n; i++)
+//     {
+//         for (int j=1; j<=i; j++)
+//         {
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     pattern3(n);
+//     return 0;
+// }
+
+//!pattern 4
+void pattern4(int n)
 {
-    for(int i=1; i<n; i++)
+    for(int i=1;i<=n;i++)
     {
-        for (int j=1; j<=i; j++)
-        {
-            cout<<j;
+        for(int j=n;j>i;j--){
+            cout<<"*";
         }
         cout<<endl;
     }
@@ -64,6 +83,7 @@ int main()
 {
     int n;
     cin>>n;
-    pattern3(n);
+    pattern4(n);
     return 0;
 }
+
