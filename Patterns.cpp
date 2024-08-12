0e761329328ab4498849139c15786566157fe60c
jagadish011 <munavallijagadish@gmail.com>
2024-08-12 13:59:22 +0530

    patterns problems

--- Patterns.cpp
+++ Patterns.cpp
@@ -458,3 +458,78 @@ using namespace std;
 //     pattern15(n);
 //     return 0;
 // }
+
+//! pattern 16
+// Input Format: N = 6
+// Result:   
+// A 
+// B B
+// C C C
+// D D D D
+// E E E E E
+// F F F F F F
+
+// void pattern16(int n)
+// {
+//     for(int i=0; i<n; i++)
+//     {
+//         char ch = 'A' + i;
+//         for(int j=0; j<=i; j++)
+//         {
+//             cout<<ch<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     pattern16(n);
+//     return 0;
+// }
+
+//! pattern 17
+
+// Input Format: N = 6
+// Result:   
+//      A     
+//     ABA    
+//    ABCBA   
+//   ABCDCBA  
+//  ABCDEDCBA 
+// ABCDEFEDCBA
+
+// void pattern17(int n)
+// {
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             cout<<" ";
+//         }
+//         char ch = 'A';
+//         int breakpoint = (2*i+1)/2;
+//         for(int j=0; j<2*i+1; j++)
+//         {
+//             cout<<ch;
+//             if(j<=breakpoint)
+//             ch++;
+//             else
+//             ch--;
+//         }
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     pattern17(n);
+//     return 0;
+// }
