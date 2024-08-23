40daae490e963771fbb45f86ef48068cf45d1e8f
jagadish011 <munavallijagadish@gmail.com>
2024-08-23 08:50:46 +0530

    recursion probemes

--- Recursion.cpp
+++ Recursion.cpp
@@ -4,12 +4,29 @@ using namespace std;
 //! Recursion
 
 //! Print Name N times using Recursion
-void func(int i,int n)
+// void func(int i,int n)
+// {
+//     // Base condition
+//     if(i>n)
+//     return;
+//     cout<<"Jaggu"<<endl;
+//     func(i+1,n);
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     func(1,n);
+//     return 0;
+// }
+
+//! Print 1 to N using Recursion
+void func(int i, int n)
 {
     // Base condition
     if(i>n)
     return;
-    cout<<"Jaggu"<<endl;
+    cout<<i<<endl;
     func(i+1,n);
 }
 int main()
@@ -19,5 +36,3 @@ int main()
     func(1,n);
     return 0;
 }
-
-//! Print 1 to N using Recursion
