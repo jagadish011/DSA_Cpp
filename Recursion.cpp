8b45dc3655fb1fc2808f920c29992a1da90381f3
jagadish011 <munavallijagadish@gmail.com>
2024-08-23 09:27:43 +0530

    recursion probemes

--- Recursion.cpp
+++ Recursion.cpp
@@ -55,17 +55,69 @@ using namespace std;
 // }
 
 // ! Print 1 to N using Recursion (not using i+1)
-void func(int i, int n)
-{
-    if(i<1)
-    return;
-    func(i-1,n);
-    cout<<i<<endl;
-}
-int main()
-{
-    int n;
-    cin>>n;
-    func(n,n);
+// void func(int i, int n)
+// {
+//     if(i<1)
+//     return;
+//     func(i-1,n);
+//     cout<<i<<endl;
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     func(n,n);
+//     // return 0;
+// }
+
+// ! Print 1 to N using Recursion (not using i+1)
+// void func(int i, int n)
+// {
+//     if(i>n);
+//     return;
+//     func(i+1,n);
+//     cout<<i<<endl;
+// }
+// int main()
+// {
+//     int n;
+//     int i;
+//     cin>>n;
+//     func(i,n);  
+//     return 0;
+// }
+
+// ! sum of n numbers
+// parametric
+// void func(int i, int sum)
+// {
+//     if(i<1){
+//     cout<<sum<<endl;
+//     return;
+//     }
+//     func(i-1,sum+i); 
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     func(n,0);
     // return 0;
-}
+// }
+// non parametric
+// int func(int n)
+// {
+//     if(n == 0)
+//     {
+//         return 0;
+//     }
+//     return n + func(n-1);
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     cout<<func(n);
+//     return 0;
+// }
+
